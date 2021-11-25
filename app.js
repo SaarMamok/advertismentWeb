9f8c2ea6b50f79c396db9e7dcac983a2abe5cf4b
SaarMamok <saarmamok63@gmail.com>
2021-11-25 23:22:19 +0200

    add images

--- app.js
+++ app.js
@@ -12,14 +12,14 @@ class Advertisement{
 
 let advertisementList=[];
 title = "Happy Hanuka";
-content = "This Hanuka in Roladin 10 Sufganyot for 10 shekels";
-style="";
-time=10000;
+content = "Happy Hanuka";
+style="https://www.hrus.co.il/wp-content/uploads/shutterstock_319097270.jpg";
+time=5000;
 period="";
 advertisementList.push(new Advertisement(title,content,style,time,period));
-advertisementList.push(new Advertisement("Happy Purim","This Purim in Roladin 10 Haman's Ear for 10 shekels",style,2000,period));
-advertisementList.push(new Advertisement("Happy Passover","This Passover in Yohananof 10 Matzot for 10 shekels",style,10000,period));
-advertisementList.push(new Advertisement("Happy Shavuot","This Shavuot in Roladin 10 Milk for 10 shekels",style,2000,period));
+advertisementList.push(new Advertisement("Happy Purim","Happy Purim","https://i.ytimg.com/vi/r4qHbKexLak/maxresdefault.jpg",2000,period));
+advertisementList.push(new Advertisement("Happy Passover","Happy Passover","https://www.gov.il/BlobFolder/news/news-25-03-21/he/news_2021_03_news-25-03-21.jpg",10000,period));
+advertisementList.push(new Advertisement("Happy Shavuot","Happy Shavuot","https://i.ytimg.com/vi/-VNC2FDtlSk/maxresdefault.jpg",2000,period));
 myJason = JSON.stringify(advertisementList);
 console.log(myJason);
 
@@ -33,6 +33,7 @@ $(document).ready(function(){
 
     function swapAdvertisements(){
         $('#advertisment_title').text(advertisementList[index++%advertisementList.length].content);
+        $('#image_advrtisment').attr('src', advertisementList[index++%advertisementList.length].style)
     }
     setInterval(swapAdvertisements, advertisementList[index++%advertisementList.length].time);
     
