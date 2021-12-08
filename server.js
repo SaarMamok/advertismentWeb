6855fee48441b72ef734247070d30adc8a368d1b
amitmes <amitmes11@gmail.com>
2021-12-08 18:06:53 +0200

    dssf

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var path =require('path')
 app.get('/screen=:num', (req, res) => {
  if(req.params.num==1){
     res.sendFile(path.join(__dirname,'index.html'))
+    res.sendFile(path.join(__dirname,'app.js'))
  }
  else if(req.params.num==2){
 
