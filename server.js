b02303cba141f8d6328f13d1e18a330ac12b7195
galpk <70628247+galpk@users.noreply.github.com>
2021-12-08 19:34:51 +0200

    3 index

--- server.js
+++ server.js
@@ -7,13 +7,13 @@ var path =require('path')
 app.use('/javascriptfiles',express.static(path.join(__dirname,'javascriptfiles')))
 app.get('/screen=:num', (req, res) => {
  if(req.params.num==1){
-    res.sendFile(path.join(__dirname,'index.html'))
+    res.sendFile(path.join(__dirname,'index1.html'))
  }
  else if(req.params.num==2){
-
+   res.sendFile(path.join(__dirname,'index2.html'))
  }
  else if(req.params.num==3){
-
+   res.sendFile(path.join(__dirname,'index3.html'))
  }
 })
 
