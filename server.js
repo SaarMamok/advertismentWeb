394c4e6a1d93e92c6ce02172f82dd6cec5211965
amitmes <amitmes11@gmail.com>
2021-12-08 18:27:51 +0200

    changes

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ var app = express()
 var port = 8080
 var path =require('path')
 
+
+app.use('/javascriptfiles',express.static(path.join(__dirname,'javascriptfiles')))
 app.get('/screen=:num', (req, res) => {
  if(req.params.num==1){
     res.sendFile(path.join(__dirname,'index.html'))
-    res.sendFile(path.join(__dirname,'app.js'))
  }
  else if(req.params.num==2){
 
