48e43085887bf84c072343c595050cc488ecd2e3
amitmes <amitmes11@gmail.com>
2022-01-07 12:50:27 +0200

    data retrived in html

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var path =require('path')
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://127.0.0.1:27017/";
 const DBNAME = "Advertisements";
-
+app.set("view engine","ejs");
 
 let advertisementList1=[
   {
@@ -64,21 +64,31 @@ MongoClient.connect(url, function(err, db) {
  ////////////////////////////////////////////////////////////
  app.use('/javascriptfiles',express.static(path.join(__dirname,'javascriptfiles')))
  app.get('/screen=:num', (req, res) => {
+  var query = {screenNumber:1};
   if(req.params.num==1){
-    var query = {screenNumber:1};
-    var result_end = null;
+  
     dbo.collection(DBNAME).find(query).toArray(function(err, result) {
         if (err) throw err;
-        res.send(result);
+        res.render("index1.ejs", {data: JSON.stringify(result)});
       });
     
     //  res.sendFile(path.join(__dirname,'index1.html'))
   }
   else if(req.params.num==2){
-    res.sendFile(path.join(__dirname,'index2.html'))
+    var query = {screenNumber:2};
+    dbo.collection(DBNAME).find(query).toArray(function(err, result) {
+      if (err) throw err;
+      res.render("index1.ejs", {data: JSON.stringify(result)});
+    });
+    // res.sendFile(path.join(__dirname,'index2.html'))
   }
   else if(req.params.num==3){
-    res.sendFile(path.join(__dirname,'index3.html'))
+    var query = {screenNumber:3};
+    dbo.collection(DBNAME).find(query).toArray(function(err, result) {
+      if (err) throw err;
+      res.render("index1.ejs", {data: JSON.stringify(result)});
+    });
+    // res.sendFile(path.join(__dirname,'index3.html'))
   }
  })
  
